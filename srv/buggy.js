07a58342811738d08a7a6f595221b257a3e29ee9
Martin Stenzig <martin.stenzig@rizing.com>
2023-03-27 17:46:52 -0700

    Bug reproduction

---
+++ srv/buggy.js
@@ -0,0 +1,37 @@
+const cds = require('@sap/cds')
+
+class BuggyReproService extends cds.ApplicationService {
+    async init() {
+
+        this.on('postWeatherData', async (req) => {
+
+            // determine url parameters from request
+            const urlParams = req._.req.query
+
+            // Output trigger information to console
+            console.log('Meteobridge request received.', urlParams);
+
+
+            // Establish connection to CDS Service
+            const serviceIotPush = await cds.connect.to('AdminService')
+
+            // Retrieve IotMessages meta data from service
+            const { IotMessages } = serviceIotPush.entities();
+            //console.log('IotMessages: ', IotMessages)
+
+            const insertStatement = INSERT.into(IotMessages, urlParams);
+            //console.log('Insert Statement: ', insertStatement)
+
+            let serviceResponse = await serviceIotPush.run(insertStatement);
+            console.log('Service Response: ', serviceResponse)
+
+            return 'inserted'
+
+        })
+
+        // ensure to call super.init()
+        await super.init()
+
+    }
+}
+module.exports = { BuggyReproService }
